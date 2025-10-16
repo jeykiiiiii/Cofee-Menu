694ed24f9c6314225c01c8bf2dd2e7fa7506d419
Jake Marlon Destura <163145636+jeykiiiiii@users.noreply.github.com>
2025-10-16 13:37:47 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,29 @@
 var coffeePrices = {
+    // Served Hot
     "Espresso": 95,
-    "Coffee 2": 6,
-    "Coffee 3": 4,
-    "Coffee 4": 7,
-    "Coffee 5": 4.5,
-    "Coffee 6": 6.5
+    "Double Espresso": 105,
+    "Red Eye": 100,
+    "Black Eye": 115,
+    "Americano": 100,
+    "Long Black": 115,
+    "Macchiato": 115,
+    "Long Macchiato": 125,
+    "Cortado": 115,
+    "Breve": 125,
+    "Cappuccino": 125,
+    "Flat White": 125,
+    "Cafe Latte": 115,
+    "Mocha": 120,
+    "Cremma": 105,
+    "Affogato": 105,
+    "Cafe au lait": 125,
+
+    // Served Cold
+    "Iced Espresso": 95,
+    "Iced Coffee": 105,
+    "Iced Flat White": 105
 };
+
 
 
 function openOrderDialog(coffeeName) {
@@ -31,16 +49,27 @@ function placeOrder() {
         alert("Please enter valid values for quantity, name, and location.");
         return;
     }
+
+    
+    function calculateTotal(coffeeName, quantity) {
+        // strip any accidental "Order " prefix
+        coffeeName = coffeeName.replace(/^Order\s*/, '');
+        var pricePerCoffee = coffeePrices[coffeeName];
+
+        if (!pricePerCoffee) {
+            console.warn("No price found for:", coffeeName);
+            return 0;
+        }
+
+        return quantity * pricePerCoffee;
+    }
+
     
     var totalValue = calculateTotal(coffeeName, quantity);
     var message = "Thank you, " + name + "! You have ordered " + quantity + " of " + coffeeName + ". Your order will be delivered to " + location + ". Total cost: $" + totalValue.toFixed(2);
     
     alert(message);
     closeOrderDialog();
-}
 
-// Calculate total value
-function calculateTotal(coffeeName, quantity) {
-    var pricePerCoffee = coffeePrices[coffeeName];
-    return pricePerCoffee ? quantity * pricePerCoffee : 0;
+    
 }
